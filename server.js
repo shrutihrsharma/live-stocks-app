7fdea5b41abc1ae363775f499471c8de820af899
Shruti <shrutihrsharma@gmail.com>
2019-03-09 04:40:22 +0530

    reconfiguring

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 // All your files and folders under public folder are deployed as root.
 app.use(express.static('public'));
 
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+//app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 app.listen(port, () => {
     console.log('App is listening on port %d', port);
